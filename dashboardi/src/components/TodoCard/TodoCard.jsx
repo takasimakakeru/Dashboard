a71fcd2f1c1643fca9517a24836ece8a95655dc7
takasimakakeru <kyunpangoogle@outlook.jp>
2026-09-05 01:03:24 +0000

    a

--- dashboardi/src/components/TodoCard/TodoCard.jsx
+++ dashboardi/src/components/TodoCard/TodoCard.jsx
@@ -56,16 +56,25 @@ export default function TodoCard() {
 		<div>
 			<div className="liquid-glass" style={{}}>
 				<div className="glass-text">
-					<input
-						value={newTodo}
-						onChange={(e) => setNewTodo(e.target.value)}
-					/>
-
-					<button onClick={addTodo}>
-						追加
-					</button>
+					
+					
 					<div className="card todo-card">
 						<h2>Todo</h2>
+<div className="inputandbutton">
+						<div class="input-wrapper">
+  	<span class="icon-left">
+    	<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#6b7280" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
+  	</span>
+  	<input
+		value={newTodo}
+		onChange={(e) => setNewTodo(e.target.value)}
+    	class="custom-input"
+    	type="text"
+    	placeholder="予定を入力..."
+  	/>
+	</div>
+	<button onClick={addTodo} class="original-button">追加</button>
+	</div>
 
 						{/* 修正：条件分岐の文字列囲みを解除 */}
 						{loading && <p>読み込み中...</p>}
