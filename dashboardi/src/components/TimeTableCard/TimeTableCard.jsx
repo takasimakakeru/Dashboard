ce69244c6d099fd9c0fab139f283fa50fc33aa01
takasimakakeru <kyunpangoogle@outlook.jp>
2026-09-09 03:55:26 +0000

    a

--- dashboardi/src/components/TimeTableCard/TimeTableCard.jsx
+++ dashboardi/src/components/TimeTableCard/TimeTableCard.jsx
@@ -1,25 +1,19 @@
-function TimeTableCard({ timetableUrl }) {
+function TimeTableCard() {
+	return (
+		<div className="liquid-glass">
+			<div className="glass-text">
+				<div className="timetable-card">
+					<h2>時間割</h2>
 
-  return (
-    <div className="liquid-glass">
-      <div className="glass-text">
-    <div className="timetable-card">
-      <h2>時間割</h2>
-
-      {timetableUrl ? (
-        <iframe
-          src={timetableUrl}
-          title="時間割表"
-          className="timetable-pdf"
-        />
-      ) : (
-        <p>時間割を取得中...</p>
-      )}
-
-    </div>
-    </div>
-    </div>
-  );
+					<iframe
+						src="https://drive.google.com/file/d/1VXc-BA1y5Y3oxN9NKAjON9ICRHxgVM2a/preview"
+						title="時間割表"
+						className="timetable-pdf"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default TimeTableCard;
